90b932e242775502481f0cf4c928bc8d6f09018d
warrior14 <ironaddicts14@gmail.com>
2021-06-03 14:36:34 -0500

    added an event listener so that when you click on the employee it will alert how many that person has sold

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,35 @@
 import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const employeeOrders = orders.filter( 
+                        (order) => {
+                            if (order.employeeId === employee.id) {
+                                return true
+                            }
+                        }
+                    )
+
+                    window.alert(`${employee.name} sold ${employeeOrders.length} product`)
+                }
+            }
+        }
+    }
+)
+
+
 
 export const Employees = () => {
     let html = "<ul>"
@@ -13,4 +42,3 @@ export const Employees = () => {
 
     return html
 }
-
