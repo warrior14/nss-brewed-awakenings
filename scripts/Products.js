eecbf5d6bc73911661c04919d1cdf92ea328bc9f
warrior14 <ironaddicts14@gmail.com>
2021-06-03 10:49:46 -0500

    Added an event listener so that the products will alert a price

--- scripts/Products.js
+++ scripts/Products.js
@@ -14,3 +14,18 @@ export const Products = () => {
     return html
 }
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [, productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(` ${product.name} costs $${product.price} `)
+                }
+            }
+        }
+    }
+)
